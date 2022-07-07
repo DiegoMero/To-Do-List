215b3a2b98cfab66953ee8d93d1b8829914da6f7
Diego Mero <diegomero_1999@hotmail.com>
2022-07-07 04:34:36 -0500

    Add to do list skeleton

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-/* eslint-disable no-undef */
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const tasks = [
+  {
+    description: 'take the dog for a walk',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'make the dinner',
+    completed: false,
+    index: 1,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const listContainer = document.querySelector('ul');
 
-  return element;
-}
+const addTask = () => {
+  for (let i = 0; i < tasks.length; i += 1) {
+    const miniListContainer = document.createElement('li');
+    miniListContainer.className = 'mini-list-container';
+    listContainer.appendChild(miniListContainer);
 
-document.body.appendChild(component());
+    const taskCheck = document.createElement('input');
+    taskCheck.className = 'task-checkbox';
+    taskCheck.type = 'checkbox';
+    taskCheck.checked = tasks[i].completed;
+    miniListContainer.appendChild(taskCheck);
+
+    const taskDescription = document.createElement('div');
+    taskDescription.innerHTML = tasks[i].description;
+    miniListContainer.appendChild(taskDescription);
+  }
+};
+
+addTask();
